b27464b063abf870b4c5d78bf7bccdd4f98dc475
braian2809 <yesidaguilera5@gmail.com>
2025-11-04 20:17:35 -0500

    jojo

--- src/app/pages/products/products.ts
+++ src/app/pages/products/products.ts
@@ -1,12 +1,21 @@
+import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Productos, Product1 } from '../../services/products';
 
 @Component({
   selector: 'app-products',
+  standalone: true,
   imports: [CommonModule],
   templateUrl: './products.html',
-  styleUrl: './products.css',
+  styleUrls: ['./products.css']
 })
 export class Products {
+  productos: Product1[] = [];
+  productosService = inject(Productos);
 
+  ngOnInit() {
+    console.log('✅ Products component cargado');
+    this.productos = this.productosService.getProducts();
+    console.log('Productos:', this.productos);
+  }
 }
--- src/app/services/products.ts
+++ src/app/services/products.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 
-export interface Products1{
+export interface Product1 {
   id: number;
   nombre: string;
   descripcion: string;
@@ -8,14 +8,35 @@ export interface Products1{
   imagen: string;
 }
 
-
-
-
-
 @Injectable({
   providedIn: 'root'
 })
-export class Products {
-  
-}
+export class Productos {
+  private productos: Product1[] = [
+    {
+      id: 1,
+      nombre: 'Camiseta JoJo’s Bizarre Adventure',
+      descripcion: 'Diseño inspirado en Jotaro Kujo.',
+      precio: 26.60,
+      imagen: 'https://i.etsystatic.com/56637758/r/il/47454c/6722960010/il_794xN.6722960010_84ez.jpg'
+    },
+    {
+      id: 2,
+      nombre: 'Figura de Dio Brando',
+      descripcion: 'Figura coleccionable de Dio Brando.',
+      precio: 3.36,
+      imagen: 'https://i.etsystatic.com/62544437/r/il/c04f89/7288410050/il_794xN.7288410050_t64x.jpg'
+    },
+    {
+      id: 3,
+      nombre: 'Manga Stone Ocean Vol. 1',
+      descripcion: 'Primer volumen del manga JoJo’s Bizarre Adventure: Stone Ocean.',
+      precio: 19.99,
+      imagen: 'https://i.etsystatic.com/53580000/r/il/f990db/6565282459/il_794xN.6565282459_d90o.jpg'
+    }
+  ];
 
+  getProducts(): Product1[] {
+    return this.productos;
+  }
+}
